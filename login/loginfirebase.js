02b796ae655ceb6105f7c72499c5a76b8f6f69a5
pranathivemireddy <pranathivemireddy22@gmail.com>
2025-04-29 15:15:37 +0530

    Login media queries added

--- login/loginfirebase.js
+++ login/loginfirebase.js
@@ -1,17 +1,38 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAbqe516BVu41W3f-mh68PJuXB0duaKuMM",
-  authDomain: "shopsphere-3d3c1.firebaseapp.com",
-  projectId: "shopsphere-3d3c1",
-  storageBucket: "shopsphere-3d3c1.firebasestorage.app",
-  messagingSenderId: "1049441510658",
-  appId: "1:1049441510658:web:d8221e625372fd84630e1b"
-};
 
 // Initialize Firebase
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNhCDyU4OgYwN9XjiCERcBmBDKlcWcy7w",
+  authDomain: "shopsphere-678f0.firebaseapp.com",
+  projectId: "shopsphere-678f0",
+  storageBucket: "shopsphere-678f0.appspot.com",
+  messagingSenderId: "503902711076",
+  appId: "1:503902711076:web:ec91fe0beceef5a4f597b1"
+};
+
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+document.getElementById("googleLogin").addEventListener("click", () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      console.log("Google Login Success:", user);
+      localStorage.setItem('LoginDetails', JSON.stringify(user));
+      setTimeout(() => {
+        window.location.href = '../index.html';
+      }, 1500);
+    })
+    .catch((error) => {
+      console.error("Google Sign-in failed:", error);
+    });
+});
